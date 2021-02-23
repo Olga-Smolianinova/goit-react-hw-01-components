4869fd68d5dbcc57b795cb8c8f82f81a5b42f2fd
Olga-Smolianinova <olga.smol2015@gmail.com>
2021-02-23 12:31:04 +0200

    Add normalize.css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import React from "react"; // import React, чтобы могли использовать createElement для создания ссылки;
+import React from 'react'; // import React, чтобы могли использовать createElement для создания ссылки;
 
-import ReactDOM from "react-dom"; // import ReactDOM чтобы Virtual DOM зарендерил что-либо в настоящий DOM;
+import ReactDOM from 'react-dom'; // import ReactDOM чтобы Virtual DOM зарендерил что-либо в настоящий DOM;
 
-import App from "./App"; //import функции App из  App.js - главного компонента (обертки) создаваемого приложения;
+import App from './App'; //import функции App из  App.js - главного компонента (обертки) создаваемого приложения;
+
+import 'modern-normalize/modern-normalize.css'; //подключение стилей для normalize
 
 // рендерим код из Аpp.js - главного компонента (обертки) создаваемого приложения;
 // ReactDOM.render(<App />, document.getElementById("root"));
@@ -12,7 +14,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
 
 // для работы с Virtual DOM (VD):
@@ -21,15 +23,15 @@ ReactDOM.render(
 // -  после этого VD превратит его в настоящий <tag-element> и вставит в DOM.
 
 // console.log(React);
-const link = React.createElement(
-  "a",
-  { href: "https://reactjs.org/" },
-  "Ссылка на reactjs.org"
-); //в параметрах указываем: 1) <tag-element>, который хотим создать; 2) дополнительные атрибуты <tag-element>. Они еще называются PROPS или свойства создаваемого будущего компонената и то, что ему будет передано; 3) CHILDREN - то, что стоит между открывающим и закрывающим тегом (<a href="">...CHILDREN...</a>)
+// const link = React.createElement(
+//   "a",
+//   { href: "https://reactjs.org/" },
+//   "Ссылка на reactjs.org"
+// ); //в параметрах указываем: 1) <tag-element>, который хотим создать; 2) дополнительные атрибуты <tag-element>. Они еще называются PROPS или свойства создаваемого будущего компонената и то, что ему будет передано; 3) CHILDREN - то, что стоит между открывающим и закрывающим тегом (<a href="">...CHILDREN...</a>)
 // console.log(link);
 
 // перепишем код для большей читабельности и удобства с помощью JSX - специальный шаблонизатор, который при помощи Babel под капотом трансформирует JSX-код в вызовы React.createElement()
-const jsxLink = <a href="https://reactjs.org/">Ссылка на reactjs.org</a>;
+// const jsxLink = <a href="https://reactjs.org/">Ссылка на reactjs.org</a>;
 // console.log(jsxLink);
 
 // Чтобы ссылку из VD перенести в настоящий DOM - ее нужно зарендерить. В Свойстве render нужно указать, что именно (link) и где (ссылка на <div id="root"></div> из index.html) нужно отобразить в реальном  DOM
